40804ced47c2827020845dabacb6404f1e153deb
Yuan Liao <liaoyuan@gmail.com>
2022-03-13 17:50:00 -0700

    Initial commit
    Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

---
+++ src/zbt/__main__.py
@@ -0,0 +1,37 @@
+#  zarro-boogs-tools Main Entry Point
+#
+#  Copyright (C) 2022 Yuan Liao
+#  Copyright (C) 2022 zarro-boogs-tools Contributors
+#
+#  This file is part of zarro-boogs-tools.
+#
+#  zarro-boogs-tools is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  zarro-boogs-tools is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with zarro-boogs-tools.  If not, see
+#  <https://www.gnu.org/licenses/>.
+
+import zbt
+
+import sys
+
+
+def main() -> int:
+    print(f"{zbt.__project_name__} {zbt.__version__}")
+    return 0
+
+
+def main_wrapper() -> None:
+    sys.exit(main())
+
+
+if __name__ == '__main__':
+    main_wrapper()
---
+++ tests/__init__.py
@@ -0,0 +1,20 @@
+#  zarro-boogs-tools Test Package Initialization File
+#
+#  Copyright (C) 2022 Yuan Liao
+#  Copyright (C) 2022 zarro-boogs-tools Contributors
+#
+#  This file is part of zarro-boogs-tools.
+#
+#  zarro-boogs-tools is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  zarro-boogs-tools is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with zarro-boogs-tools.  If not, see
+#  <https://www.gnu.org/licenses/>.
